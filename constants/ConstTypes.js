4dc2e0f4868740299c9662c87c8d7f32462cbb4f
zhoudy <zhoudy@rd.netease.com>
2018-09-04 19:59:01 +0800

    add tags img

--- constants/ConstTypes.js
+++ constants/ConstTypes.js
@@ -1,4 +1,57 @@
+
 export const RoleType = {
   1: '管理员',
   10: '普通用户'
 };
+
+export const TagsArray = [
+  {
+    name: 'HTML',
+    url: '/static/tags/HTML.png'
+  }, {
+    name: 'JavaScript',
+    url: '/static/tags/JavaScript.png'
+  }, {
+    name: 'CSS',
+    url: '/static/tags/CSS.png'
+  }, {
+    name: '前端',
+    url: '/static/tags/WEB.png'
+  }, {
+    name: 'React',
+    url: '/static/tags/React.png'
+  }, {
+    name: 'Vue',
+    url: '/static/tags/Vue.png'
+  }, {
+    name: 'Angular',
+    url: '/static/tags/Angular.png'
+  }, {
+    name: 'Webpack',
+    url: '/static/tags/Webpack.png'
+  }, {
+    name: 'Nodejs',
+    url: '/static/tags/Nodejs.png'
+  }, {
+    name: '后端',
+    url: '/static/tags/Back.png'
+  }, {
+    name: 'Java',
+    url: '/static/tags/Java.png'
+  }, {
+    name: '产品',
+    url: '/static/tags/Product.png'
+  }, {
+    name: '数据库',
+    url: '/static/tags/Database.png'
+  }, {
+    name: '算法',
+    url: '/static/tags/Algorithm.png'
+  }, {
+    name: 'Python',
+    url: '/static/tags/Python.png'
+  }, {
+    name: 'Http',
+    url: '/static/tags/Http.png'
+  },
+];
