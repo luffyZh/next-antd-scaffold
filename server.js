cccdcc51c22c212d542f9b7389d0ec3ce3d0dfe7
luffyZhou <zhoudeyou945@126.com>
2018-10-11 10:38:55 +0800

    fix server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const { parse } = require('url');
 const next = require('next');
 
 const port = parseInt(process.env.PORT, 10) || 3006;
-
+// 判断开发环境和生产环境
 process.env.NODE_ENV = (typeof process.env.NODE_ENV !== 'undefined')
   ? process.env.NODE_ENV.trim()
   : 'development';
-
-const dev = process.env.NODE_ENV.trim() !== 'production';
+const dev = process.env.NODE_ENV !== 'production';
 
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -19,8 +19,14 @@ app.prepare()
     server.get('/user/userDetail', (req, res) => {
       return app.render(req, res, `/user/userDetail/${req.query.username}`);
     });
-    
+
     server.get('*', (req, res) => {
+      const parsedUrl = parse(req.url, true);
+      const { pathname } = parsedUrl;
+      if (typeof pathname !== 'undefined' && pathname.indexOf('/user/userDetail/') > -1) {
+        const query = { username: pathname.split('/')[3] };
+        return app.render(req, res, '/user/userDetail', query);
+      }
       return handle(req, res);
     });
 
