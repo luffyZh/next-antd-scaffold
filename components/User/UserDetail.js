c2741bd060f02b5c2fc7b5e4d293705dcc06431d
luffyZhou <zhoudeyou945@126.com>
2019-09-26 16:25:08 +0800

    beautify code

--- components/User/UserDetail.js
+++ components/User/UserDetail.js
@@ -1,22 +1,11 @@
-import { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-class UserDetail extends Component {
-  static propTypes = {
-    router: PropTypes.object.isRequired
-  }
-  constructor(props) {
-    super(props);
-    const { router: { query } } = props;
-    this.state = { username: query.username };
-  }
 
-  render() {
-    return (
-      <Fragment>
-        <h1>用户信息：{this.state.username}</h1>
-      </Fragment>
-    );
-  }
-}
+const UserDetail = ({ router }) => (
+  <h1>用户信息：{router.query.username}</h1>
+);
 
 export default UserDetail;
+
+UserDetail.propTypes = {
+  router: PropTypes.object.isRequired
+};
--- components/User/UserList.js
+++ components/User/UserList.js
@@ -1,67 +1,33 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Table } from 'antd';
 import Link from 'next/link';
-import { RoleType } from '../../constants/ConstTypes';
 
-class UserList extends Component {
-  static propTypes = {
-    isServer: PropTypes.bool.isRequired,
-    fetchUserListData: PropTypes.func.isRequired,
-    list: PropTypes.array.isRequired
-  }
-  constructor(props) {
-    super(props);
-    const { list } = props;
-    this.state = {
-      dataSource: list
-    };
+const columns = [{
+  title: 'Username',
+  dataIndex: 'username',
+  key: 'username',
+  render: (text) => (
+    <Link href={`/user/detail?username=${text}`} as={`/user/detail/${text}`}>
+      <a>{text}</a>
+    </Link>
+  )
+}, {
+  title: 'Email',
+  dataIndex: 'email',
+  key: 'email',
+}];
 
-    this.columns = [{
-      title: 'Username',
-      dataIndex: 'username',
-      key: 'username',
-      render: (text) => (
-        <Link href={`/user/detail?username=${text}`} as={`/user/detail/${text}`}>
-          <a>{text}</a>
-        </Link>
-      )
-    }, {
-      title: 'Email',
-      dataIndex: 'email',
-      key: 'email',
-    }, {
-      title: 'Role',
-      dataIndex: 'role',
-      key: 'role',
-      render: (text) => <span>{RoleType[text]}</span>
-    }];
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.list && nextProps.list !== prevState.dataSource) {
-      return {
-        dataSource: nextProps.list
-      };
-    }
-    return null; 
-  }
-
-  render() {
-    const { dataSource } = this.state;
-    dataSource.map(item => {
-      item.key = item.id;
-      item.role = 10;
-    });
-    return (
-      <Table
-        style={{ minWidth: '600px' }}
-        dataSource={dataSource}
-        columns={this.columns}
-        bordered
-      />
-    );
-  }
-}
+const UserList = ({ list }) => (
+  <Table
+    style={{ minWidth: '600px' }}
+    dataSource={list}
+    columns={columns}
+    bordered
+  />
+);
 
 export default UserList;
+
+UserList.propTypes = {
+  list: PropTypes.array.isRequired
+};
