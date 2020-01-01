9194fe39ab024dd92564df30e799c3b5832d00b2
luffyZhou <zhoudeyou945@126.com>
2020-01-01 23:43:58 +0800

    minimize code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import getConfig from 'next/config';
@@ -7,79 +6,61 @@ import { color_primary } from '../constants/CustomTheme';
 // Only holds serverRuntimeConfig and publicRuntimeConfig from next.config.js nothing else.
 const { publicRuntimeConfig: { staticFolder } } = getConfig();
 
-class Header extends Component {
-  static propTypes = {
-    title: PropTypes.string
-  }
-  static defaultProps = {
-    title: ''
-  }
-  constructor(props) {
-    super(props);
-    const { title } = props;
-    this.state = { title };
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.title !== prevState.title) {
-      return {
-        title: nextProps.title
-      };
-    }
-    return null;
-  }
-
-  render() {
-    const { title } = this.state;
-    return (
-      <div id='header_bar' className='container'>
-        <style jsx>{`
-          .container {
-            position: fixed;
-            top: 0;
-            width: 100%;
-            height: 60px;
-            background-color: ${color_primary};
-            z-index: 999;
-          }
-          h1 {
-            text-align: center;
-            line-height: 60px;
-            font-size: 1.6rem;
-            font-weight: 500;
-            color: #fff;
-          }
-          .logo-container {
-            position: absolute;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            top: 15px;
-            left: 20px;
-            cursor: pointer;
-          }
-          .sys-name {
-            display: inline-block;
-            margin-left: 10px;
-            font-size: 20px;
-            color: #fff;
-            font-weight: 600;
-          }
-          .logo {
-            width: 30px;
-            height: 30px;
-          }
-        `}</style>
-        <Link href='/'>
-          <div className='logo-container'>
-            <img className='logo' alt='logo' src={`${staticFolder}/logo.png`} />
-            <span className='sys-name'>Next-Antd-Scaffold</span>
-          </div>
-        </Link>
-        <h1>{title}</h1>
+const Header = ({ title }) => (
+  <div id='header_bar' className='container'>
+    <style jsx>{`
+      .container {
+        position: fixed;
+        top: 0;
+        width: 100%;
+        height: 60px;
+        background-color: ${color_primary};
+        z-index: 999;
+      }
+      h1 {
+        text-align: center;
+        line-height: 60px;
+        font-size: 1.6rem;
+        font-weight: 500;
+        color: #fff;
+      }
+      .logo-container {
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        top: 15px;
+        left: 20px;
+        cursor: pointer;
+      }
+      .sys-name {
+        display: inline-block;
+        margin-left: 10px;
+        font-size: 20px;
+        color: #fff;
+        font-weight: 600;
+      }
+      .logo {
+        width: 30px;
+        height: 30px;
+      }
+    `}</style>
+    <Link href='/'>
+      <div className='logo-container'>
+        <img className='logo' alt='logo' src={`${staticFolder}/logo.png`} />
+        <span className='sys-name'>Next-Antd-Scaffold</span>
       </div>
-    );
-  }
-}
+    </Link>
+    <h1>{title}</h1>
+  </div>
+);
 
 export default Header;
+
+Header.propTypes = {
+  title: PropTypes.string
+};
+
+Header.defaultProps = {
+  title: ''
+};
