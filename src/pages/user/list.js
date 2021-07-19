40dc4840683aee5e55b34d196d8137a3b0d1dccd
zhoudeyou <zhoudeyou@corp.netease.com>
2021-07-19 14:56:38 +0800

    :lipstick: feat: use next newest api

--- src/pages/user/list.js
+++ src/pages/user/list.js
@@ -3,11 +3,13 @@ import { wrapper } from '@/redux/store';
 import UserList from '@/components/User/UserList';
 import { fetchUserList } from '@/redux/actions/user';
 
-export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
-  store.dispatch(fetchUserList());
-  store.dispatch(END);
-  await store.sagaTask.toPromise();
-});
+export const getServerSideProps = wrapper.getServerSideProps(
+  async ({ store, req, res }) => {
+    store.dispatch(fetchUserList());
+    store.dispatch(END);
+    await store.sagaTask.toPromise();
+  }
+);
 
 export default UserList;
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,10 +5,12 @@ import userMiddleware from '@/middlewares/client/user';
 import rootReducer from './reducers/index';
 import rootSaga from './sagas/index';
 
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
 function bindMiddleware(middleware) {
   // add route middleware
   middleware.push(userMiddleware);
-  if (process.env.NODE_ENV !== 'production') {
+  if (!IS_PRODUCTION) {
     const { composeWithDevTools } = require('redux-devtools-extension');
     // development use logger
     const { logger } = require('redux-logger');
@@ -28,7 +30,7 @@ function configureStore () {
   store.sagaTask = sagaMiddleware.run(rootSaga);
 
   // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
-  if (process.env.NODE_ENV !== 'production' && module.hot) {
+  if (!IS_PRODUCTION && module.hot) {
     module.hot.accept('./reducers', function() {
       // eslint-disable-next-line global-require
       store.replaceReducer(require('./reducers').default);
@@ -40,4 +42,4 @@ function configureStore () {
 
 export default configureStore;
 
-export const wrapper = createWrapper(configureStore, { debug: true });
+export const wrapper = createWrapper(configureStore, { debug: !IS_PRODUCTION });
